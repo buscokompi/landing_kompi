f52ed7eb06ce98690e3c4ad161ae9d6b34e1ced0
Antonio Bogallo <antonio.bogallo@gmail.com>
2022-03-29 16:43:37 +0200

    guardado2_cabecera

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ function iniciarApp() {
 }
 
 function navegacionFija() {
-  const header = document.querySelector(".contenedor");
-  const aux = document.querySelector(".header");
+  const header = document.querySelector(".header");
+  const aux = document.querySelector(".section-1");
 
   window.addEventListener("scroll", function() {
     if (aux.getBoundingClientRect().bottom < 0) {
