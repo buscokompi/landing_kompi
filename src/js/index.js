f90cd7496f66f1f15589d417a4af5cdde57bb36f
Jose Fernando Alvarez <joseferalvarezromero@gmail.com>
2022-03-31 14:31:02 +0200

    section dos anadida y cambios en la topbar

--- src/js/index.js
+++ src/js/index.js
@@ -9,15 +9,17 @@ function iniciarApp() {
 function navegacionFija() {
   const header = document.querySelector(".header");
   const section = document.querySelector(".section-1");
-  const aux = document.querySelector(".section-1");
+  // const aux = document.querySelector(".section-1");
 
   window.addEventListener("scroll", function() {
-    if (aux.getBoundingClientRect().top < 0) {
+    if (section.getBoundingClientRect().top < 0) {
       header.classList.add("fijo");
       section.classList.add("padding");
+      header.classList.add("backbar");
     } else {
       header.classList.remove("fijo");
       section.classList.remove("padding");
+      header.classList.remove("backbar");
     }
   });
 }
