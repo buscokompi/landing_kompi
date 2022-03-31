249f26b39fd865bd019cc54c2afb9ce1318845e8
Jose Fernando Alvarez <joseferalvarezromero@gmail.com>
2022-03-31 16:00:46 +0200

    cambios en el funcionamiento de scroll de la topbar

--- src/js/index.js
+++ src/js/index.js
@@ -9,16 +9,16 @@ function iniciarApp() {
 function navegacionFija() {
   const header = document.querySelector(".header");
   const section = document.querySelector(".section-1");
-  // const aux = document.querySelector(".section-1");
+  const aux = document.querySelector(".section-1");
 
   window.addEventListener("scroll", function() {
     if (section.getBoundingClientRect().top < 0) {
       header.classList.add("fijo");
-      section.classList.add("padding");
+      aux.classList.add("padding");
       header.classList.add("backbar");
     } else {
       header.classList.remove("fijo");
-      section.classList.remove("padding");
+      aux.classList.remove("padding");
       header.classList.remove("backbar");
     }
   });
