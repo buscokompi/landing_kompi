0dc64af5a504b0aa4af2535f0a080570617fe1e1
Antonio Bogallo <antonio.bogallo@gmail.com>
2022-04-06 22:16:04 +0200

    rama_kevin

---
+++ src/js/imageblur.js
@@ -0,0 +1,16 @@
+const imagenes = document.querySelectorAll(".containerimg");
+
+function hoverimages(image) {
+  for (let i = 0; i < imagenes.length; i++) {
+    imagenes[i].classList.add("blur");
+  }
+  image.classList.add("zoom");
+  image.classList.remove("blur");
+}
+
+function hoverout(image) {
+  for (let i = 0; i < imagenes.length; i++) {
+    imagenes[i].classList.remove("blur");
+    imagenes[i].classList.remove("zoom");
+  }
+}
