5bc040275f76bc13d21f839b16f568e9c70de973
Juani <jrm3301@gmail.com>
2022-04-08 21:25:37 +0100

    responsive, media queries, (y modificaciones del header)

--- src/js/index.js
+++ src/js/index.js
@@ -24,4 +24,12 @@ function navegacionFija() {
       header.classList.add("backbar2");
     }
   });
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 }
