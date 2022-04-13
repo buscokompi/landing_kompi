4ede0329813e3ed41afcd3615406899a21b5d3ba
Jose Fernando Alvarez <joseferalvarezromero@gmail.com>
2022-04-13 11:42:55 +0200

    Cambios en el script de blur de las imagenes

--- src/js/imageblur.js
+++ src/js/imageblur.js
@@ -1,4 +1,9 @@
-const imagenes = document.querySelectorAll(".grid-container");
+const imagenes = document.getElementsByClassName("grid-container");
+
+for (let i = 0; i < imagenes.length; i++) {
+  imagenes[i].addEventListener("mouseover", () => hoverimages(imagenes[i]));
+  imagenes[i].addEventListener("mouseout", hoverout);
+}
 
 function hoverimages(image) {
   for (let i = 0; i < imagenes.length; i++) {
@@ -8,7 +13,7 @@ function hoverimages(image) {
   image.classList.remove("blur");
 }
 
-function hoverout(image) {
+function hoverout() {
   for (let i = 0; i < imagenes.length; i++) {
     imagenes[i].classList.remove("blur");
     imagenes[i].classList.remove("zoom");
