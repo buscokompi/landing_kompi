469613cbc7b8da234640a4dda17543d872693076
“Alexis <alexisplettener@gmail.com>
2022-05-04 16:48:48 +0100

    hamburguesa terminada

--- src/js/imageblur.js
+++ src/js/imageblur.js
@@ -1,23 +1,22 @@
 const imagenes = document.getElementsByClassName("grid-container");
 
 for (let i = 0; i < imagenes.length; i++) {
-  imagenes[i].addEventListener("mouseover", () => hoverimages(imagenes[i]));
-  imagenes[i].addEventListener("mouseout", hoverout);
+    imagenes[i].addEventListener("mouseover", () => hoverimages(imagenes[i]));
+    imagenes[i].addEventListener("mouseout", hoverout);
 }
 
 function hoverimages(image) {
-  for (let i = 0; i < imagenes.length; i++) {
-    imagenes[i].classList.add("blur");
-  }
-  image.classList.remove("pepe");
-  image.classList.add("zoom");
-  image.classList.remove("blur");
-  
+    for (let i = 0; i < imagenes.length; i++) {
+        imagenes[i].classList.add("blur");
+    }
+    image.classList.remove("pepe");
+    image.classList.add("zoom");
+    image.classList.remove("blur");
 }
 
 function hoverout() {
-  for (let i = 0; i < imagenes.length; i++) {
-    imagenes[i].classList.remove("blur");
-    imagenes[i].classList.remove("zoom");
-  }
+    for (let i = 0; i < imagenes.length; i++) {
+        imagenes[i].classList.remove("blur");
+        imagenes[i].classList.remove("zoom");
+    }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,44 +1,21 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//   iniciarApp();
-// });
-
-// function iniciarApp() {
-//   navegacionFija();
-// }
-
-// function navegacionFija() {
-//   const header = document.querySelector(".topbar");
-//   const section = document.querySelector(".section-1");
-//   // const aux = document.querySelector(".section-1");
-
-//   window.addEventListener("scroll", function() {
-//     if (section.getBoundingClientRect().top < 0) {
-//       // header.classList.add("fixed");
-//       // aux.classList.add("padding");
-//       header.classList.add("topbar-lower");
-//       header.classList.remove("topbar-upper");
-//     } else {
-//       header.classList.remove("fijo");
-//       // aux.classList.remove("padding");
-//       header.classList.remove("topbar-lower");
-//       header.classList.add("topbar-upper");
-//     }
-//   });
-// }
-// // se han comentado varias partes del .js al descubrir que el padding era el que causaba el salto de la barra de navegación al hacer scroll.
-
+// Header transparent to white when scroll
 window.addEventListener("scroll", function () {
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-document.getElementById("menuClick").onclick = function () { toggleMenu(); };
+// Burger menu for mobile
+
+const click = document.querySelectorAll(".menuClick");
+click.forEach(element => {
+    element.addEventListener("click", toggleMenu);
+});
 
 function toggleMenu() {
     const menuToggle = document.querySelector(".menu-toggle");
-    const navigation = document.querySelector(".navigation");
+    const navigation = document.querySelector(".mobile-navigation");
     const logo = document.querySelector(".logo");
-    const menuButton = document.querySelector(".menuButton");
+    const menuButton = document.querySelector(".menu-button");
 
     menuToggle.classList.toggle("active");
     navigation.classList.toggle("active");
