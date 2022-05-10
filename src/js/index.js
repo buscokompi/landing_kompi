0de46791d32ca4a3bece21111b159082b1af2575
“Alexis <alexisplettener@gmail.com>
2022-05-10 18:11:35 +0100

    renovación landing con respecto a estructura de la main

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,20 @@
 // Header transparent to white when scroll
 window.addEventListener("scroll", function () {
-    const header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
+    const topbar = document.querySelector(".topbar");
+    topbar.classList.toggle("sticky", window.scrollY > 0);
 });
 
 // Burger menu for mobile
 
-const click = document.querySelectorAll(".menuClick");
+const click = document.querySelectorAll(".burger-img");
 click.forEach(element => {
     element.addEventListener("click", toggleMenu);
 });
 
 function toggleMenu() {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navigation = document.querySelector(".mobile-navigation");
-    const logo = document.querySelector(".logo");
+    const burger = document.querySelector(".burger");
+    const burgerImg = document.querySelector(".burger-img");
 
-    menuToggle.classList.toggle("active");
-    navigation.classList.toggle("active");
-    logo.classList.toggle("active");
+    burger.classList.toggle("active");
+    burgerImg.classList.toggle("active");
 }
